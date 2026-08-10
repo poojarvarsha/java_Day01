1795a92e67656922178057746020a8887439c3ff
Varsha P <poojarvarsha117@gmail.com>
2026-08-10 19:04:57 +0530

    Day01_3rdactivity

---
+++ Multiname1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Multiname1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name 1: ");
+        String name1 = sc.nextLine();
+
+        System.out.print("Enter name 2: ");
+        String name2 = sc.nextLine();
+
+        System.out.print("Enter name 3: ");
+        String name3 = sc.nextLine();
+
+        System.out.print("Enter name 4: ");
+        String name4 = sc.nextLine();
+
+        System.out.print("Enter name 5: ");
+        String name5 = sc.nextLine();
+
+        System.out.println("\nNames entered are:");
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name4);
+        System.out.println(name5);
+
+    }
+}
